d24a6a7b37888df1111dadba0418fdabc6c03418
Aurélien <au2001sfg@gmail.com>
2021-09-15 12:43:23 +0200

    Initial Commit

---
+++ EfreiAutoLogin/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  EfreiAutoLogin
+//
+//  Created by Aurélien Garnier on 15/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
